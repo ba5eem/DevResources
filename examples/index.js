d23f9e6c1eac9b2dbebe9bc45965a4e234e70ca7
ba5eem <baseem@gmail.com>
2018-08-03 09:46:03 -1000

    project update

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,16 @@
 
-
-let url = "https://www.reddit.com/r/gifrecipes.gif"
-let src;
-
-let x = url.substring(url.length-1);
-if (x === 'v'){
-  // cut off v
-  src = url.substring(0, url.length-1);
-  console.log(src);
+function hand(){
+  console.log('ola1')
+  console.log(this)
 }
-else{
-  src = url;
-  console.log(src);
-}
+
+let client = new XMLHttpRequest();
+
+client.open("GET", "https://www.reddit.com/r/random.json", true);
+//client.setRequestHeader('Access-Control-Allow-Headers', '*');
+//client.setRequestHeader('Content-type', 'application/ecmascript');
+client.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+client.onreadystate = hand;
+client.send();
+
